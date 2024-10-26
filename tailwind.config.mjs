623cb05cba9f5a9328b19428c204dcb9b8d2a003
Anson Chung <hi@anscg.net>
2024-10-26 12:18:47 +0800

    Add writings page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				popupfromdownwards: {
+					'0%': { opacity: 0, transform: 'translateY(70%)', filter: 'blur(40px)' },
+					'100%': { opacity: 1, transform: 'translateY(0)', filter: 'blur(0px)' },
+				}
+			},
+			animation: {
+				'popupd1': 'popupfromdownwards 0.8s cubic-bezier(0.34, 1.56, 0.64, 1) 2s forwards',
+				'popupd2': 'popupfromdownwards 0.8s cubic-bezier(0.34, 1.56, 0.64, 1) 2.1s forwards',
+			}
+		},
 	},
 	plugins: [],
 }
