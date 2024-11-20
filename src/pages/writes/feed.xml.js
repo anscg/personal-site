2c81694ccc683c370c67b17a5baf448523f10cf6
Anson Chung <hi@anscg.net>
2024-11-21 01:10:36 +0800

    style

--- src/pages/writes/feed.xml.js
+++ src/pages/writes/feed.xml.js
@@ -1,11 +1,15 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET(context) {
     const writes = await getCollection('writes');
   return rss({
     // `<title>` field in output xml
     title: 'Anson Chung Writes',
+    stylesheet: '/pretty-feed-v3.xsl',
     // `<description>` field in output xml
     description: 'I write about stuff.',
     // Pull in your project "site" from the endpoint context
@@ -20,6 +24,10 @@ export async function GET(context) {
         // Compute RSS link from post `slug`
         // This example assumes all posts are rendered as `/blog/[slug]` routes
         link: `/writes/${post.slug}/`,
+        content: sanitizeHtml(parser.render(post.body), {
+            allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+          }),
+    
       })),
     // (optional) inject custom xml
     customData: `<language>en-us</language>`,
