c707df508aba18acfe6ca70be0433ddb2677037d
Anson Chung <58066418+anscg@users.noreply.github.com>
2025-04-25 16:10:43 +0000

    ee

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: 'https://anscg.net',
   integrations: [tailwind(), sitemap()],
-  redirects: [
-    { source: '/c', destination: 'https://bento.me/anscg', permanent: true }
-  ]
+  redirects: {
+    '/c': { destination: 'https://bento.me/anscg', permanent: true }
+  }
 });
