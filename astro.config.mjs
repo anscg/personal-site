63c43ff90af17f2159bbca1f242ea4acaad77695
Anson Chung <58066418+anscg@users.noreply.github.com>
2025-04-25 16:07:23 +0000

    ez

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://anscg.net',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap()],
+  redirects: [
+    { source: '/c', destination: 'https://bento.me/anscg', permanent: true }
+  ]
 });
